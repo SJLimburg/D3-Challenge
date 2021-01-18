0bd81f4b2e5320c3284f32a43633933571ae260f
SJLimburg <forestgirl6@hotmail.com>
2021-01-18 15:28:43 -0500

    commit start

--- d3_data_journalism/StarterCode/assets/js/app.js
+++ d3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,14 @@
 // @TODO: YOUR CODE HERE!
+let width = parseInt(d3.select("#scatter").style("width"));
+console.log(width);
+
+//place scatter plot in <div id="scatter">
+
+
+//read data from csv files
+
+// filepath = "../data/data.csv";
+
+// function print(d => console.log(d));
+
+// myData = d3.csv(filepath).then(print());
